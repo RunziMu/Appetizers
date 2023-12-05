4e09c91033d90108a6fcfbe78953ba75baed764b
itseliorz <rmu6@uwo.ca>
2023-12-05 16:38:54 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Runzi Mu on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
